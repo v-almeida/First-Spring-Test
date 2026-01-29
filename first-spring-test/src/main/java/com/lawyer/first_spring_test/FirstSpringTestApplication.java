d2ae53a0a7df8c4908a436b121d1e7ba67aa0a7f
v-almeida <almeida.vitor20000@gmail.com>
2026-01-29 19:08:31 -0300

    First Spring Test

---
+++ first-spring-test/src/main/java/com/lawyer/first_spring_test/FirstSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.lawyer.first_spring_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringTestApplication.class, args);
+	}
+
+}
---
+++ first-spring-test/src/test/java/com/lawyer/first_spring_test/FirstSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawyer.first_spring_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
