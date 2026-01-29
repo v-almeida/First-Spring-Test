063c9b31d7ae034a46f5a4df257ee204157a6fc7
v-almeida <almeida.vitor20000@gmail.com>
2026-01-29 19:13:28 -0300

    importing some frame works

--- first-spring-test/src/main/java/com/lawyer/first_spring_test/FirstSpringTestApplication.java
+++ first-spring-test/src/main/java/com/lawyer/first_spring_test/FirstSpringTestApplication.java
@@ -1,9 +1,16 @@
 package com.lawyer.first_spring_test;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+
 public class FirstSpringTestApplication {
 
 	public static void main(String[] args) {
