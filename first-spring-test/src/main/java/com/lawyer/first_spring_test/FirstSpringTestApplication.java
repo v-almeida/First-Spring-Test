9475c9c76fddbe9855e705f3f9922587b756f525
v-almeida <almeida.vitor20000@gmail.com>
2026-02-02 20:22:36 -0300

    excluded and requested

--- first-spring-test/src/main/java/com/lawyer/first_spring_test/FirstSpringTestApplication.java
+++ first-spring-test/src/main/java/com/lawyer/first_spring_test/FirstSpringTestApplication.java
@@ -15,7 +15,6 @@ public class FirstSpringTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstSpringTestApplication.class, args);
-		System.out.println("Hello, World!");
 	}
 
 }
