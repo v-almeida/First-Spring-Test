a993545cd627dc79e4bcfe264b0923c23ac6491a
v-almeida <almeida.vitor20000@gmail.com>
2026-02-05 19:47:23 -0300

    Injeção de dependência do próprio spring para mudar a frase da página!

--- first-spring-test/src/main/java/com/lawyer/first_spring_test/controller/HelloWorldController.java
+++ first-spring-test/src/main/java/com/lawyer/first_spring_test/controller/HelloWorldController.java
@@ -4,14 +4,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lawyer.first_spring_test.services.HelloWorldServices;
+
 @RestController
 @RequestMapping("/hello-world")
 
 public class HelloWorldController {
 
+    private HelloWorldServices helloWorldServices;
+
+    public HelloWorldController(HelloWorldServices helloWorldServices){
+        this.helloWorldServices = helloWorldServices;
+    }
+
     // GET /hello-world
     @GetMapping
     public String helloWorld() {
-        return "Hello, World!";
+        return helloWorldServices.helloWorld();
     }
 }
---
+++ first-spring-test/src/main/java/com/lawyer/first_spring_test/services/HelloWorldServices.java
@@ -0,0 +1,15 @@
+package com.lawyer.first_spring_test.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class HelloWorldServices {
+
+    public String helloWorld(){
+        return "You are not a REAL LAWYER!";
+    }
+
+    
+
+}
